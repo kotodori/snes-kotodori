4b4d1816c2d9b777e23fcf86371a567ac68dbcea
Daisuke Yamamura <mucho613@gmail.com>
2020-12-06 17:53:54 +0900

    WIP: fix indent

--- src/main.asm
+++ src/main.asm
@@ -95,57 +95,57 @@ mainloop:
 .endproc
 
 .proc VBlank
-    pha
-    phx
-    php
+  pha
+  phx
+  php
 
 
-    sep #$20
+  sep #$20
 .a8
-    lda scrollX
-    sta $210D
-    inc scrollX
+  lda scrollX
+  sta $210D
+  inc scrollX
 
-    rep #$20
+  rep #$20
 .a16
 
-    plp
-    plx
-    pla
-    rti
+  plp
+  plx
+  pla
+  rti
 .endproc
 
 .proc EmptyInt
-    rti
+  rti
 .endproc
 
 ; カートリッジ情報
 .segment "TITLE"
-    .byte "KOTODORI             " ; Game Title
+  .byte "KOTODORI             " ; Game Title
 .segment "HEADER"
-    .byte $31                   ; 0x01:HiRom, 0x30:FastRom(3.57MHz)
-    .byte $00                   ; ROM only
-    .byte $08                   ; 32KB=256KBits
-    .byte $00                   ; RAM Size (8KByte * N)
-    .byte $00                   ; NTSC
-    .byte $01                   ; Licensee
-    .byte $00                   ; Version
-    .byte $9a, $46, $65, $b9    ; checksum(empty here)
-    .byte $ff, $ff, $ff, $ff    ; unknown
+  .byte $31                   ; 0x01:HiRom, 0x30:FastRom(3.57MHz)
+  .byte $00                   ; ROM only
+  .byte $08                   ; 32KB=256KBits
+  .byte $00                   ; RAM Size (8KByte * N)
+  .byte $00                   ; NTSC
+  .byte $01                   ; Licensee
+  .byte $00                   ; Version
+  .byte $9a, $46, $65, $b9    ; checksum(empty here)
+  .byte $ff, $ff, $ff, $ff    ; unknown
 
-    .word EmptyInt              ; Native:COP
-    .word EmptyInt              ; Native:BRK
-    .word EmptyInt              ; Native:ABORT
-    .word VBlank                ; Native:NMI
-    .word $0000                 ;
-    .word EmptyInt              ; Native:IRQ
+  .word EmptyInt              ; Native:COP
+  .word EmptyInt              ; Native:BRK
+  .word EmptyInt              ; Native:ABORT
+  .word VBlank                ; Native:NMI
+  .word $0000                 ;
+  .word EmptyInt              ; Native:IRQ
 
-    .word $0000
-    .word $0000
+  .word $0000
+  .word $0000
 
-    .word EmptyInt              ; Emulation:COP
-    .word EmptyInt              ;
-    .word EmptyInt              ; Emulation:ABORT
-    .word VBlank                ; Emulation:NMI
-    .word Reset                 ; Emulation:RESET
-    .word EmptyInt              ; Emulation:IRQ/BRK
+  .word EmptyInt              ; Emulation:COP
+  .word EmptyInt              ;
+  .word EmptyInt              ; Emulation:ABORT
+  .word VBlank                ; Emulation:NMI
+  .word Reset                 ; Emulation:RESET
+  .word EmptyInt              ; Emulation:IRQ/BRK
