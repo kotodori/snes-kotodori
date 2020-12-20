bea84bc9d2ec80eeb399886a63a6216e3566ba55
Ryota Egusa <egusa.ryota@gmail.com>
2020-12-20 15:03:38 +0900

    Show 2P controller

--- src/controller.asm
+++ src/controller.asm
@@ -8,39 +8,70 @@
 
 .export printControllerInput
 function printControllerInput
-  rep #$20
+  rep #$30
   .a16
+  .i16
 
   lda #$0000
   pha
   plb
 
+  .define buffer1 $03
+  .define buffer2 $01
   lda gControllerInput1
-  .define buffer $01
-  sta buffer
+  pha
+  lda gControllerInput2
+  pha
+  tsx
+
+  phd
+  lda #$0000
+  pha
+  pld
+
+  .define ascii0 $0030
+  .define ascii1 $0031
+
 
   lda #$41a9
   sta rVRamAddress
 
-  .i16
   ldy #$0010
-
-  copyInput:
-    .define ascii0 $0030
-    .define ascii1 $0031
+  loop1:
     lda #ascii0
 
-    asl buffer
-    bcc store
+    asl buffer1, x
+    bcc store1
 
     lda #ascii1
 
-    store:
-    sta $2118
+    store1:
+    sta rVRamDataWrite
 
     dey
-    bne copyInput
+    bne loop1
 
+
+  lda #$41e9
+  sta rVRamAddress
+
+  ldy #$0010
+  loop2:
+    lda #ascii0
+
+    asl buffer2, x
+    bcc store2
+
+    lda #ascii1
+
+    store2:
+    sta rVRamDataWrite
+
+    dey
+    bne loop2
+
+
+  pld
 endFunction
 
 .export readControllerInputs
@@ -54,26 +85,31 @@ function readControllerInputs
   plb
 
   lda #$01
-  sta $4016
+  sta rNesControllerPort1
 
-  stz $4016
+  stz rNesControllerPort1
   nop
   nop
 
   ldx #$0010
   loop:
-    lda $4016
-
+    lda rNesControllerPort1
     rep #$20
     .a16
-
     lsr
     rol gControllerInput1
+
+    sep #$20
+    .a8
+    lda rNesControllerPort2
+    rep #$20
+    .a16
+    lsr
+    rol gControllerInput2
 
     sep #$20
     .a8
 
     dex
     bne loop
-
 endFunction
